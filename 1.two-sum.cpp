5314d60b902514c1996b1527383ba5f21caeae74
JING <jeanwang325@gmail.com>
2017-09-16 22:00:41 -0500

    two-sum

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
-	sort(sums.begin(), sums.end(), sums);
-	int headp = 0; //head index
-	int tailp = sums.size()-1; // tail index
-	while(headp < tailp){
-	     if(sums[headp] + sums[tailp] == target){
-		res.push_back(headp);
-		res.push_back(tailp);
-		break;
-	     }
-	     else if(sums[headp]+sums[tailp] > target)
-		tailp--;
-	     else
-		headp++;
-	}
-	return res;	
-        
+        unordered_map<int,int> hash_t; // hash table
+        for(int i = 0; i < nums.size(); ++i){
+            hash_t[nums[i]] = i;
+        }
+        for(int i = 0; i < nums.size(); ++i){
+            int residual = target - nums[i];
+            if(hash_t.find(residual)!=hash_t.end() && hash_t[residual] > i){
+                res.push_back(i);
+                res.push_back(hash_t[residual]);
+                break;
+            }
+        }
+        return res;        
     }
 };
